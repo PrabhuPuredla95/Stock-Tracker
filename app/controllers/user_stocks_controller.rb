e2be328a81959331c5b960102d11fe7f7c7a1bfd
CW-prabhupuredla <prabhu.puredla3@gmail.com>
2019-12-31 18:34:45 +0530

    friends and final change

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,12 +1,18 @@
-class UserStocksController : ApplicationController
+class UserStocksController < ApplicationController
   def create
     stock = Stock.find_by_ticker(params[:stock_ticker])
     if stock.blank?
-      stock = stock.new_from_lookup(params[:stock_ticker])
+      stock = Stock.new_from_lookup(params[:stock_ticker])
       stock.save
     end
     @user_stock = UserStock.create(user: current_user, stock: stock)
     flash[:success] = "Stock #{@user_stock.stock.name} was successfully added to portfolio"
+    redirect_to my_portfolio_path
+  end
+  def destroy
+    stock = UserStock.where(user_id: current_user.id, stock_id: params[:id]).first
+    stock.destroy
+    flash[:notice] = "Stock was successfully removed from portfolio"
     redirect_to my_portfolio_path
   end
 end
